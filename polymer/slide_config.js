a5675f61cf42f93365d0b4ac1c01c737b53543f0
jefferykao <jefferykao@kkbox.com>
2014-07-15 16:52:24 +0800

    upload slide

---
+++ polymer/slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Introduction to <br>Web Component',
+    subtitle: '初心者的web component介紹',
+    eventInfo: {
+      title: 'Tech talk',
+      date: '7/18/2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/KKBOX_app_icon/KKBOX_app_icon.png',//'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Jeffery Kao',
+    company: 'KKBOX',
+    gplus: '',
+    twitter: '',
+    www: '',
+    github: 'http://github.com/jeffery0316/'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
